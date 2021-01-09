8bfacd95d7b9d9900b5bb49ebdb0822c30ec6b3e
BakareHalimat <halimatbakare10@gmail.com>
2021-01-09 13:20:27 +0100

    Created random scores for each student

--- Program.cs
+++ Program.cs
@@ -12,15 +12,45 @@ namespace ArraysandList
             Random randNum = new Random();
             int[] studentscores = new int[randNum.Next(35,100)];
             Random rand_Num = new Random();
+            Console.WriteLine("");
+            Console.WriteLine("Below are the scores of the students");
             for (int i = 0; i < studentscores.Length; i++)
             {
                 studentscores[i] = rand_Num.Next(Min, Max);
-               // Console.WriteLine(studentscores[i]);
+                Console.WriteLine(studentscores[i]);
                 int scores = studentscores[i];
-                //int[] newscores = new int[scores];
                 
-                //Console.WriteLine("The total number of student in class is " + newscores.);
             }
-                
+            Console.WriteLine("");
+            Console.WriteLine("The total number of student in class is " + studentscores.Length ); 
+            Console.WriteLine("");
+            Console.WriteLine("The list of scores of student below 50 "); 
+            foreach (var item in studentscores)
+            {
+                 if (item <= 50)
+                 {
+                    Console.WriteLine(item + "");
+                 }
+           }
+            Console.WriteLine("");
+            Console.WriteLine("The list of scores of student between 50 and 69 "); 
+            foreach (var item in studentscores)
+            {
+                 if (item >= 50 && item <= 69)
+                 {
+                    Console.WriteLine(item);
+                 }
+           }
+            Console.WriteLine("");
+            Console.WriteLine("The list of scores of student from 70 and above "); 
+            foreach (var item in studentscores)
+            {
+                 if (item >= 70)
+                 {
+                    Console.WriteLine(item);
+                 }
+           }
+
+
     }   }
 }
--- int.cs
+++
@@ -1,3 +0,0 @@
-
-            int[] result = item.ToString().Select(o=>Convert.ToInt32(o)).ToArray();
-            Console.WriteLine(result.Length);
